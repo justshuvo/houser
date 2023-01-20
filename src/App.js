ae1c7aa093603469cd3515c4fea01be6bed7e9b7
Shuvo Sarker <34802984+justshuvo@users.noreply.github.com>
2023-01-20 17:02:08 +0600

    making usable directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 const App = () => {
     return (
         <div className='App'>
-            <h1>App Component</h1>
+            <h1 className="">App Component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+                quibusdam veritatis non cum reprehenderit magnam eius voluptatem
+                tenetur! Numquam distinctio blanditiis consequuntur provident
+                tempore doloribus aperiam ea eius eveniet et.
+            </p>
         </div>
     );
 };
